3a6e15ee43c0f3ce0f8979433695c2e8f56f23c1
latyfo <latyfo@gmail.com>
2017-12-24 11:41:46 +0100

    MovinAverageFilter Squeleton

--- src/Api/Controllers/Dht11SensorController.php
+++ src/Api/Controllers/Dht11SensorController.php
@@ -2,13 +2,17 @@
 
 namespace Src\Api\Controllers;
 use Src\Api\Services\Dht11SensorService;
+use Src\Api\Filters\MovinAverageFilter;
 
 class Dht11SensorController extends JsonController implements Controller
 {
     private $service;
+    private $movinAverageFilter;
+
     public function __construct (){
         parent::__construct();
         $this->service = Dht11SensorService::getInstance();
+        $this->movinAverageFilter = new MovinAverageFilter();
     }
     public function findAll($limit = 50){
         $objects = $this->service->findAll($limit);
@@ -20,8 +24,12 @@ class Dht11SensorController extends JsonController implements Controller
     }
     public function save(){
         $object = parent::getDataFromJsonHeader();
-        $result = $this->service->save($object);
-        $this->respondSuccess("sensor mesure saved", $result);
+        // $object = $this->movinAverageFilter->filter($object);
+        if ($object != NULL) {
+            $result = $this->service->save($object);
+            $this->respondSuccess("sensor mesure saved", $result);
+        }
+        $this->respondSuccess("sensor mesure saved", 15);
     }
 
     public function update()
